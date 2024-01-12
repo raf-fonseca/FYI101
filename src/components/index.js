b0d02f6609b03e22b0a36deb83e2431dd0dc66ec
SilkyJesus <rafael.silvanofonseca@gmail.com>
2024-01-11 22:05:55 -0500

    Home page

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import WaitList from "./WaitList";
 import Business from "./Business";
 import Footer from "./Footer";
 import Hero from "./Hero";
+import GetStarted from "./GetStarted";
 
-export { Navbar, WaitList, Business, Footer, Hero };
+export { Navbar, WaitList, Business, Footer, Hero, GetStarted };
