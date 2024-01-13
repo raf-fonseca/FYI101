60775554415fe9e4122d858aa1ad0dfbab1b23c2
SilkyJesus <rafael.silvanofonseca@gmail.com>
2024-01-13 10:15:06 -0500

    test

--- src/components/WaitList.jsx
+++ src/components/WaitList.jsx
@@ -6,7 +6,7 @@ const WaitList = () => (
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
         Sign Up For
-        <br className="sm:block hidden" />
+        <br className="sm:block hidden mt-5" />
         The Wait List
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5 mb-10 `}>
