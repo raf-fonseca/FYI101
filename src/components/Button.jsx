42bdb2605ff198ea126bfdae51a09c96135c4deb
SilkyJesus <rafael.silvanofonseca@gmail.com>
2024-01-13 11:44:56 -0500

    Updated button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Button = ({ styles }) => {
   return (
-    <button
-      type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
-    >
-      Get Started
-    </button>
+    <a href="#waitList">
+      <button
+        type="button"
+        className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+      >
+        Get Started
+      </button>
+    </a>
   );
 };
 
