65dff36a16d3f3bc1b4f95db8176c9934e0c5b95
SilkyJesus <rafael.silvanofonseca@gmail.com>
2024-02-24 14:44:08 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,44 +1,40 @@
 import { pathIcon, plusIcon, calendarIcon } from "../assets";
 
 export const navLinks = [
-  {
-    id: "home",
-    title: "Home",
-  },
-  {
-    id: "features",
-    title: "Features",
-  },
-  {
-    id: "waitList",
-    title: "Wait List",
-  },
-  {
-    id: "contactUs",
-    title: "Contact Us",
-  },
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "features",
+        title: "Features",
+    },
+    {
+        id: "waitList",
+        title: "Wait List",
+    },
 ];
 
 export const features = [
-  {
-    id: "feature-1",
-    icon: calendarIcon,
-    title: "Scheduling",
-    content:
-      "Input your course sylabus and get a personalized schedule with important due dates.",
-  },
-  {
-    id: "feature-2",
-    icon: pathIcon,
-    title: "Personalized Path",
-    content:
-      "Get a personalized path towards your goals by inputting your program and interests.",
-  },
-  {
-    id: "feature-3",
-    icon: plusIcon,
-    title: "Health & Wellness",
-    content:
-      "AI will using Machine Learning to get to know you and provide resources based on your needs  .",
-  },
+    {
+        id: "feature-1",
+        icon: calendarIcon,
+        title: "Scheduling",
+        content:
+            "Input your course sylabus and get a personalized schedule with important due dates.",
+    },
+    {
+        id: "feature-2",
+        icon: pathIcon,
+        title: "Personalized Path",
+        content:
+            "Get a personalized path towards your goals by inputting your program and interests.",
+    },
+    {
+        id: "feature-3",
+        icon: plusIcon,
+        title: "Health & Wellness",
+        content:
+            "AI will using Machine Learning to get to know you and provide resources based on your needs.",
+    },
 ];
